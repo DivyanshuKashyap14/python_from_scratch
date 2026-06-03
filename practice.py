b7b0401d5563e1e36480b094add0e045eb625469
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-06-03 22:54:22 +0530

    66 question solved

--- practice.py
+++ practice.py
@@ -563,3 +563,20 @@
 #     if i > maximum:
 #         maximum = i
 # print(maximum)
+
+n = int(input("Enter a number: "))
+
+original = n
+digits = len(str(n))
+
+total = 0
+
+while n > 0:
+    digit = n % 10
+    total += digit ** digits
+    n = n // 10
+
+if total == original:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
