b8c3861b9d6004ee55b91c7cb1ffdac41329f24c
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-10 14:44:40 +0530

    Learning

--- exception_handling.py
+++ exception_handling.py
@@ -1,6 +1,17 @@
-a=int(input("n="))
-try:
-    print(10/a)
-except ZeroDivisionError:
-    print("cant divide by 0")
-print("division done")
+#a=int(input("n="))
+#try:
+#    print(10/a)
+#except ZeroDivisionError:
+#    print("cant divide by 0")
+#print("division done")
+
+
+
+# n=int(input("n="))
+# try:
+#     print(10/n)
+# except ZeroDivisionError:
+#     print("cant divide by 0")
+# else:
+#     print("no exceptions")
+# print("division done")
