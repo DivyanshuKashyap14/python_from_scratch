83758d9f7add9081a082ed2c4d8b4228c4eff920
DivyanshuKashyap14 <divyanshukashyap140904@gmail.com>
2026-07-10 14:53:20 +0530

    Learning

--- exception_handling.py
+++ exception_handling.py
@@ -15,3 +15,14 @@
 # else:
 #     print("no exceptions")
 # print("division done")
+
+
+# age=int(input("Tell you age:"))
+# try:
+#     if age<10 or age>18:
+#         raise ValueError("Your age must be 10 & 18")
+#     else:
+#         print("Welcome to the club")
+# except Exception as err:
+#     print(f"an error occured as {err}")
+# print("Stay tuned")
